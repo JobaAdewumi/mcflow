8a05a119514ce88929d5ec3c23a5d6f2f60cbfe1
Joba Adewumi <jobaadewumis@gmail.com>
2022-04-10 11:32:18 +0100

    Removed some config

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,11 @@ import { join } from 'path';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: parseInt(<string>process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      // host: process.env.MYSQL_HOST,
+      // port: parseInt(<string>process.env.MYSQL_PORT),
+      // username: process.env.MYSQL_USER,
+      // password: process.env.MYSQL_PASSWORD,
+      // database: process.env.MYSQL_DATABASE,
       autoLoadEntities: true,
       synchronize: false, // TODO: remove when entering production
       url: process.env.DATABASE_URL,
