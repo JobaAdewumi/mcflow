9baf0c8b3d439f6cda01c311c65894b8a968e25c
Joba Adewumi <jobaadewumis@gmail.com>
2022-04-11 18:56:09 +0100

    changed the coupon vendors

--- mcflow/src/app/home/components/dashboard-vendors-modal/dashboard-vendors-modal.component.ts
+++ mcflow/src/app/home/components/dashboard-vendors-modal/dashboard-vendors-modal.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-vendors-modal',
   templateUrl: './dashboard-vendors-modal.component.html',
-  styleUrls: ['./dashboard-vendors-modal.component.scss']
+  styleUrls: ['./dashboard-vendors-modal.component.scss'],
 })
 export class DashboardVendorsModalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
